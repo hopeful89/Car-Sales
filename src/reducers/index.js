c68f2e3dc2ee503246b6d585b4a7fc0ea5c4d54a
Brandon Stine <you@example.com>
2020-07-28 14:40:05 -0400

    commit for computer swap

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,23 +18,26 @@ const initialState = {
 };
 
 export const dummyReducer = (state = initialState, action) => {
+  console.log(state)
   switch (action.type) {
     case ADD_FEATURE:
       //TODO add conditional to keep the state from changing on additional adds will need to compare ids
       return {
         ...state,
         car: {...state.car, features: [...state.car.features, action.payload] },
-        additionalPrice: state.additionalPrice += action.payload.price
+        additionalPrice: state.additionalPrice += action.payload.price,
+        additionalFeatures: state.additionalFeatures.filter(obj => obj.id !== action.payload.id)
       };
     case REMOVE_FEATURE:
       console.log('this is remove feature', action.payload.id)
-      //should be very similar to add just using a filter of state.car.features
+      //TODO: should be very similar to add just using a filter of state.car.features
       //make sure to pass in the object to compare a name/id
       //dont forget to remove the price from the total as well
       return {
         ...state,
         car: {...state.car, features: state.car.features.filter(obj=> obj.id !== action.payload.id) },
-        additionalPrice: state.additionalPrice -= action.payload.price
+        additionalPrice: state.additionalPrice -= action.payload.price,
+        additionalFeatures: [...state.additionalFeatures, action.payload]//could add sort for state to keep them in same order based on id/name/etc
       };
     default:
       return state;
